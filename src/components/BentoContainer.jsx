56155faab738e90d39b41eb7f9f9f153d1528ad0
Tecfabi <tecfabi@mail.com>
2024-02-23 14:23:18 -0300

    include responsive design

--- src/components/BentoContainer.jsx
+++ src/components/BentoContainer.jsx
@@ -4,6 +4,7 @@ import pyflag from '../assets/pyflag.png'
 import { techs } from '../assets/techs'
 import ProyectCard from './ProyectCard'
 import { proyectsList } from '../proyects'
+import MediaQuery from 'react-responsive'
 
 function BentoContainer () {
   const nameStyle = {
@@ -19,33 +20,53 @@ function BentoContainer () {
   }
 
   return (
-    <main className='bento-container container' id='bento'>
-      <BentoItem columns='1/3' rows='1/3'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
-      <BentoItem columns='3/5' rows='1/4' img='https://www.alucoildesign.com/assets/pages/media/profile/people19.png' />
-      <BentoItem columns='5/7' rows='1/3'><ProyectCard proyect={proyectsList[0]} /></BentoItem>
-      <BentoItem columns='1/3' rows='3/6'><ProyectCard proyect={proyectsList[0]} /></BentoItem>
-      <BentoItem columns='3/5' rows='4/5'>
-        <h2 style={nameStyle}>{'< Oliver Amarilla />'} </h2>
-      </BentoItem>
-      <BentoItem columns='3/4' rows='5/6'>
-        <p style={descriptionStyle}>
-          Front-end <span style={{ color: '#70FF00' }}>{'</>'}</span> developer
-        </p>
-      </BentoItem>
-      <BentoItem columns='4/5' rows='5/6' img={pyflag} />
-      <BentoItem columns='5/6' rows='3/4'>
-        <img className='logo' src={techs.javascript} alt='javascript' />
-      </BentoItem>
-      <BentoItem columns='6/7' rows='3/4'>
-        <img className='logo' src={techs.react} alt='react' />
-      </BentoItem>
-      <BentoItem columns='5/7' rows='4/6'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
-      <BentoItem columns='1/7' rows='6/7' className='about' align='left'>
-        <h3 className='title'>aboutMe<span>( )</span></h3>
-        <p>Passionate about technology from an early age, with outstanding skills in the assembly, installation and configuration of various electronic devices. Committed to proactive assistance and always willing to learn and help.</p>
-      </BentoItem>
+    <>
 
-    </main>
+      <main className='bento-container container hide-mq' id='bento'>
+        <BentoItem columns='1/3' rows='1/3'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='3/5' rows='1/4' img='https://www.alucoildesign.com/assets/pages/media/profile/people19.png' />
+        <BentoItem columns='5/7' rows='1/3'><ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='1/3' rows='3/6'><ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='3/5' rows='4/5'>
+          <h2 style={nameStyle}>{'< Oliver Amarilla />'} </h2>
+        </BentoItem>
+        <BentoItem columns='3/4' rows='5/6'>
+          <p style={descriptionStyle}>
+            Front-end <span style={{ color: '#70FF00' }}>{'</>'}</span> developer
+          </p>
+        </BentoItem>
+        <BentoItem columns='4/5' rows='5/6' img={pyflag} />
+        <BentoItem columns='5/6' rows='3/4'>
+          <img className='logo' src={techs.javascript} alt='javascript' />
+        </BentoItem>
+        <BentoItem columns='6/7' rows='3/4'>
+          <img className='logo' src={techs.react} alt='react' />
+        </BentoItem>
+        <BentoItem columns='5/7' rows='4/6'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='1/7' rows='6/7' className='about' align='left'>
+          <h3 className='title'>aboutMe<span>( )</span></h3>
+          <p>Passionate about technology from an early age, with outstanding skills in the assembly, installation and configuration of various electronic devices. Committed to proactive assistance and always willing to learn and help.</p>
+        </BentoItem>
+      </main>
+
+      <MediaQuery query='(max-width: 709px)'>
+        <main className='bento-container-mq container' id='bento'>
+          <BentoItem columns='1/3' rows='1/4' img='https://www.alucoildesign.com/assets/pages/media/profile/people19.png' />
+
+          <BentoItem columns='1/2'>
+            <p style={{ ...descriptionStyle, fontSize: '30px', paddingTop: '20px' }}>
+              Front-end <span style={{ color: '#70FF00' }}>{'</>'}</span> developer
+            </p>
+          </BentoItem>
+          <BentoItem columns='2/3' img={pyflag} />
+          <BentoItem columns='1/3' className='about' align='left'>
+            <h3 className='title'>aboutMe<span>( )</span></h3>
+            <p>Passionate about technology from an early age, with outstanding skills in the assembly, installation and configuration of various electronic devices. Committed to proactive assistance and always willing to learn and help.</p>
+          </BentoItem>
+
+        </main>
+      </MediaQuery>
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 export default function Header () {
+  const handleTp = (e) => {
+    e.preventDefault()
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' })
+  }
   return (
     <header id='header'>
       <h1><span className='brace'>{'{ }'} </span>OLIVER<span className='amarilla'>AMARILLA</span></h1>
-      <nav>
+      <nav className='hide-mq'>
         <a href='https://www.linkedin.com/in/oliver-amarilla/' target='_blank' className='btn' rel='noreferrer'><i className='fa-brands fa-linkedin' /> LinkedIn</a>
         <a className='btn' href='https://github.com/Oliveramarilla14' target='_blank' rel='noreferrer'><i className='fa-brands fa-github' /> GitHub</a>
-        <a className='btn contact'><i className='fa-regular fa-envelope' /> Contact me</a>
+        <a onClick={handleTp} className='btn contact'><i className='fa-regular fa-envelope' /> Contact me</a>
 
       </nav>
     </header>
