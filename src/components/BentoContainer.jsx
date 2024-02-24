01bacaf4908a289fb82086e2c9544e3e0827f6f3
Tecfabi <tecfabi@mail.com>
2024-02-24 12:35:45 -0300

    added more projects

--- src/components/BentoContainer.jsx
+++ src/components/BentoContainer.jsx
@@ -26,8 +26,8 @@ function BentoContainer () {
       <main className='bento-container container hide-mq' id='bento'>
         <BentoItem columns='1/3' rows='1/3'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
         <BentoItem columns='3/5' rows='1/4' img={porfileWeb} />
-        <BentoItem columns='5/7' rows='1/3'><ProyectCard proyect={proyectsList[0]} /></BentoItem>
-        <BentoItem columns='1/3' rows='3/6'><ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='5/7' rows='1/3'><ProyectCard proyect={proyectsList[1]} /></BentoItem>
+        <BentoItem columns='1/3' rows='3/6'><ProyectCard proyect={proyectsList[2]} /></BentoItem>
         <BentoItem columns='3/5' rows='4/5'>
           <h2 style={nameStyle}>{'< Oliver Amarilla />'} </h2>
         </BentoItem>
@@ -45,7 +45,7 @@ function BentoContainer () {
         <BentoItem columns='6/7' rows='3/4'>
           <img className='logo' src={techs.react} alt='react' />
         </BentoItem>
-        <BentoItem columns='5/7' rows='4/6'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='5/7' rows='4/6'> <ProyectCard proyect={proyectsList[3]} /></BentoItem>
         <BentoItem columns='1/7' rows='6/7' className='about' align='left'>
           <h3 className='title'>aboutMe<span>( )</span></h3>
           <p>Passionate about technology from an early age, with outstanding skills in the assembly, installation and configuration of various electronic devices. Committed to proactive assistance and always willing to learn and help.</p>
--- src/proyects.js
+++ src/proyects.js
@@ -1,14 +1,41 @@
 import crudRedux from './assets/redux-crud.png'
+import quotesApp from './assets/quotesapp.png'
+import quizApp from './assets/quizapp.png'
+import ageCalcApp from './assets/agecalculator.png'
 import { techs } from './assets/techs'
 
 export const proyectsList = [
   {
     title: 'ToDo List',
     img: crudRedux,
-    url: 'https://olivertodolist.netlify.app/',
-    github: 'https://github.com/Oliveramarilla/todo-list',
+    url: 'https://olivertodoapp.netlify.app/',
+    github: 'https://github.com/oliveramarilla14/todo',
     techs: [techs.javascript, techs.react, techs.tailwind],
-    description: 'ToDo list with options to CRUD using React, Redux Toolkit, Router and tailwind'
+    description: 'ToDo list with options to CRUD using React, Redux Toolkit, Router and tailwind.'
+  },
+  {
+    title: 'Age Calculator',
+    img: ageCalcApp,
+    url: 'https://oliveragecalc.netlify.app/',
+    github: 'https://github.com/oliveramarilla14/agecalculator',
+    techs: [techs.javascript, techs.react],
+    description: 'Front-end mentor challenge with react.'
+  },
+  {
+    title: 'Daily Quotes',
+    img: quotesApp,
+    url: 'https://oliverquotes.netlify.app/',
+    github: 'https://github.com/oliveramarilla14/Quotes',
+    techs: [techs.html5, techs.sass, techs.javascript],
+    description: 'Daily quotes with API fetch and twitter share.'
+  },
+  {
+    title: 'Quiz App',
+    img: quizApp,
+    url: 'https://oliverquiz.netlify.app/',
+    github: 'https://github.com/oliveramarilla14/quiz',
+    techs: [techs.html5, techs.sass, techs.javascript],
+    description: 'Quiz app with step form and final score.'
   }
 
 ]
