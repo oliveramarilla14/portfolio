d303aed5a804c49193e1112bd350994eb5c83c1a
oliver <oamarilla.informatica@centralshop.com.py>
2024-09-04 11:25:00 -0400

    agregar efectos a bento

--- src/components/BentoContainer.jsx
+++ src/components/BentoContainer.jsx
@@ -24,29 +24,29 @@ function BentoContainer () {
     <>
 
       <main className='bento-container container hide-mq' id='bento'>
-        <BentoItem columns='1/3' rows='1/3'> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
-        <BentoItem columns='3/5' rows='1/4' img={porfileWeb} />
-        <BentoItem columns='5/7' rows='1/3'><ProyectCard proyect={proyectsList[1]} /></BentoItem>
-        <BentoItem columns='1/3' rows='3/6'><ProyectCard proyect={proyectsList[2]} /></BentoItem>
-        <BentoItem columns='3/5' rows='4/5'>
+        <BentoItem columns='1/3' rows='1/3' animate='left' transition={{ duration: 1.25, delay: 0.25 }}> <ProyectCard proyect={proyectsList[0]} /></BentoItem>
+        <BentoItem columns='3/5' rows='1/4' img={porfileWeb} animate='top' transition={{ duration: 1.25, delay: 1.25 }} />
+        <BentoItem columns='5/7' rows='1/3' animate='right' transition={{ duration: 1.25, delay: 0.50 }}><ProyectCard proyect={proyectsList[1]} /></BentoItem>
+        <BentoItem columns='1/3' rows='3/6' animate='left' transition={{ duration: 1.25, delay: 0.65 }}><ProyectCard proyect={proyectsList[2]} /></BentoItem>
+        <BentoItem columns='3/5' rows='4/5' animate='bottom' transition={{ duration: 1.25, delay: 1 }}>
           <h2 style={nameStyle}>{'< Oliver Amarilla />'} </h2>
         </BentoItem>
-        <BentoItem columns='3/4' rows='5/6'>
+        <BentoItem columns='3/4' rows='5/6' animate='bottom' transition={{ duration: 1.25, delay: 0.85 }}>
           <p style={descriptionStyle}>
             Full-Stack <span style={{ color: '#70FF00' }}>{'</>'}</span> developer
           </p>
         </BentoItem>
-        <BentoItem columns='4/5' rows='5/6'>
+        <BentoItem columns='4/5' rows='5/6' animate='bottom' transition={{ duration: 1.25, delay: 0.85 }}>
           <img className='logo' src={pyflag} alt='Paraguay flag' />
         </BentoItem>
-        <BentoItem columns='5/6' rows='3/4'>
+        <BentoItem columns='5/6' rows='3/4' animate='left'>
           <img className='logo' src={techs.Laravel} alt='javascript' />
         </BentoItem>
-        <BentoItem columns='6/7' rows='3/4'>
+        <BentoItem columns='6/7' rows='3/4' animate='right'>
           <img className='logo' src={techs.React} alt='react' />
         </BentoItem>
-        <BentoItem columns='5/7' rows='4/6'> <ProyectCard proyect={proyectsList[3]} /></BentoItem>
-        <BentoItem columns='1/7' rows='6/7' className='about' align='left'>
+        <BentoItem columns='5/7' rows='4/6' animate='bottom'> <ProyectCard proyect={proyectsList[3]} /></BentoItem>
+        <BentoItem columns='1/7' rows='6/7' className='about' align='left' animate='bottom'>
           <h3 className='title'>aboutMe<span>( )</span></h3>
           <p>3 years of experience as a Software Developer. Led front-end projects and migrations from legacy systems to modern ones, analyzing requirements with the development team to optimize delivery times and the quality of our products.</p>
         </BentoItem>
--- src/components/BentoItem.jsx
+++ src/components/BentoItem.jsx
@@ -1,4 +1,6 @@
-function BentoItem ({ children, columns, img, rows, align = 'center', className }) {
+import { motion } from 'framer-motion'
+
+function BentoItem ({ children, columns, img, rows, align = 'center', className, animate, transition }) {
   const style = {
     gridColumn: columns,
     gridRow: rows,
@@ -7,13 +9,54 @@ function BentoItem ({ children, columns, img, rows, align = 'center', className 
     backgroundSize: 'cover',
     textAlign: align
   }
+
+  const generatePostionX = (position) => {
+    switch (position) {
+      case 'left':
+        return -200
+      case 'right':
+        return 200
+      default:
+        return 0
+    }
+  }
+
+  const generatePostionY = (position) => {
+    switch (position) {
+      case 'top':
+        return -200
+      case 'bottom':
+        return 200
+      default:
+        return 0
+    }
+  }
+
+  const initial = {
+    opacity: 0,
+    x: generatePostionX(animate),
+    y: generatePostionY(animate)
+  }
   return (
-    <div
+    <motion.div
       style={style}
       className={`bento-item ${className}`}
+      variants={{
+        left: {
+          opacity: 1,
+          x: 0
+        }
+      }}
+      initial={initial}
+      animate={{
+        opacity: 1,
+        x: 0,
+        y: 0
+      }}
+      transition={{ duration: 1.25, ease: 'circOut', ...transition }}
     >
       {children}
-    </div>
+    </motion.div>
   )
 }
 
