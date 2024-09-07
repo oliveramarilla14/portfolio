8aca0fdb0525d007bbe4ff5bbb55995a7ecbfda3
oliver <oamarilla.informatica@centralshop.com.py>
2024-09-07 11:51:46 -0400

    solve video issue

--- src/components/ProyectModal.jsx
+++ src/components/ProyectModal.jsx
@@ -49,7 +49,7 @@ export default function ProyectModal({ proyect, setSelectedProyect }) {
           <video
             style={{
               width: '100%',
-              height: '100%',
+              height: '80%',
               objectFit: 'cover'
             }}
             controls
